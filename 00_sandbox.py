f3bea2a0a37fac9d0651dece9bd5da06b91c3ccf
RyanCliff <cliff.hanna@awatapu.school.nz>
2021-07-05 13:05:56 +1200

    start code

---
+++ 00_sandbox.py
@@ -0,0 +1,8 @@
+# ask user for a number.
+number = int(input("Choose a number? "))
+
+# multiply the number by 5.
+answer = number * 5
+
+# output result.
+print("{} times 5 = ".format(number, answer))
