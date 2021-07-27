5f3484abf305cbb0f7912ee898cf5cb2a4c44d4a
RyanCliff <cliff.hanna@awatapu.school.nz>
2021-07-27 13:12:49 +1200

    Small Update

--- 01.yesno_checker_v1.py
+++ 01.yesno_checker_v1.py
@@ -1,6 +1,6 @@
 answer= " "
 while answer.lower() != "xxx":
-    answer = input("Have you played the Lucky Unicorn game before?").lower()
+    answer = input("Have you played the Lucky Unicorn game before? ").lower()
     if answer == "yes" or answer == "y":
         print("start game")
     elif answer == "no" or answer == "n":
