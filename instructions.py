0dd3f5b5a7cecde964dd6f78ce6ce2c24ab42c8c
RyanCliff <cliff.hanna@awatapu.school.nz>
2021-07-30 09:50:05 +1200

    First version

---
+++ instructions.py
@@ -0,0 +1,28 @@
+# Make Function
+def yes_no(question):
+    valid = False
+    while not valid:
+        answer = input(question).lower()
+        if answer == "yes" or answer == "y":
+            print("start game")
+            return answer
+        elif answer == "no" or answer == "n":
+            print("display instructions")
+            return answer
+        else:
+            print("Please pick yes or no.")
+
+def instructions():
+    print("***How To Play***")
+    print()
+    print("The rules of the game are.")
+    print()
+    return ""
+
+
+# Main Routine
+show_instructions = yes_no("Have you played the Lucky Unicorn game before? ")
+if show_instructions == "yes":
+    print("Display instructions")
+else:
+    print("Program continues")
