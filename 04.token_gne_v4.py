37111c3aadd3d8264829af318e9264200209a409
Ryan Cliff <cliff.hanna@awatapu.school.nz>
2021-08-13 09:38:22 +1200

    code update v4

--- 04.token_gne_v4.py
+++ 04.token_gne_v4.py
@@ -1,18 +1,25 @@
 import random
 
-tokens = ["unicorn", "horse", "horse", "horse", "zebra", "zebra", "zebra", "donkey", "donkey", "donkey"]
 STARTING_BALANCE = 100
 
 balance = STARTING_BALANCE
-for item in range(0,500):
-    chosen = random.choice(tokens)
+for item in range(0, 10):
+    chosen_num = random.randint(1, 100)
 
-    if chosen == "unicorn":
+    if 1 <= chosen_num <= 5:
+        chosen = "unicorn"
         balance += 4
-        print(balance)
-    elif chosen == "horse" or chosen == "zebra":
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
         balance -= 0.5
-        print(balance)
     else:
-        balance -= 1
-        print(balance)
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+            balance -= 0.5
+        else:
+            chosen = "zebra"
+            balance -= 0.5
+    print("You got a {} your balance is ${:.2f}".format(chosen, balance))
+
+print("Starting balance: ${}".format(STARTING_BALANCE))
+print("Final balance: ${:.2f}".format(balance))
